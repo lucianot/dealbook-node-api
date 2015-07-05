b60b40fe46629897cb6a646a06e28ca60f2861a1
Luciano Tavares <lucianomt@gmail.com>
2015-07-05 15:32:00 -0300

    Adds GET /company/:id route and tests.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var Company = require('./../models/Company');
 
 router.get('/api/v1/companies', getAll);
+router.get('/api/v1/company/:id', getOne);
 
 function getAll(req, res) {
   Company.find(function(err, companies) {
@@ -10,6 +11,19 @@ function getAll(req, res) {
       res.send(err);
 
     res.json(companies);
+  });
+}
+
+function getOne(req, res) {
+  Company.findById(req.params.id, function(err, company) {
+    if (err)
+      res.send(err);
+
+    if (company) {
+      res.json(company);
+    } else {
+      res.json();
+    }
   });
 }
 
--- test/routes.test.js
+++ test/routes.test.js
@@ -7,6 +7,7 @@
 var sinon = require('sinon');
 var expect = require('chai').expect;
 var request = require('supertest');
+var mongoose = require('mongoose');
 var app = require('./../index');
 var utils = require('./utils');
 var Company = require('./../models/Company');
@@ -18,19 +19,19 @@ describe('routes', function() {
 
   describe('GET /companies', function() {
     it('returns all companies', function(done) {
-      Company.create({ name: 'Magnetis' });
-
-      request(url)
-        .get('/api/v1/companies')
-        .end(function(err, res) {
-          if (err) {
-            throw err;
-          }
-          expect(res.status).to.eql(200);
-          expect(res.body.length).to.eql(1);
-          expect(res.body[0].name).to.eql('Magnetis');
-          done();
-        });
+      Company.create({ name: 'Magnetis' }, function(err, companies) {
+        request(url)
+          .get('/api/v1/companies')
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            expect(res.status).to.eql(200);
+            expect(res.body.length).to.eql(1);
+            expect(res.body[0].name).to.eql('Magnetis');
+            done();
+          });
+      });
     });
 
     it('returns empty array when no companies', function(done) {
@@ -43,6 +44,38 @@ describe('routes', function() {
           expect(res.status).to.eql(200);
           expect(res.body.length).to.eql(0);
           expect(res.body).to.eql([]);
+          done();
+        });
+    });
+  });
+
+  describe('GET /companies/:id', function() {
+    it('returns one company', function(done) {
+      Company.create({ name: 'Magnetis' }, function(err, company) {
+        request(url)
+          .get('/api/v1/company/'+ company._id)
+          .end(function(err, res) {
+            if (err) {
+              throw err;
+            }
+            expect(res.status).to.eql(200);
+            expect(res.body.name).to.eql('Magnetis');
+            done();
+          });
+      });
+    });
+
+    it('returns empty when company not found', function(done) {
+      var id = new mongoose.Types.ObjectId();
+
+      request(url)
+        .get('/api/v1/company/' + id)
+        .end(function(err, res) {
+          if (err) {
+            throw err;
+          }
+          expect(res.status).to.eql(200);
+          expect(res.body).to.be.empty;
           done();
         });
     });
