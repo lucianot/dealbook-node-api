22f9e7c9b7e72014d416083a9eb102fd154a639f
Luciano Tavares <lucianomt@gmail.com>
2015-07-04 22:12:23 -0300

    Creates Company model.

---
+++ models/Company.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+var CompanySchema = new mongoose.Schema({
+  name: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Company', CompanySchema);
---
+++ test/company.test.js
@@ -0,0 +1,23 @@
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+var mongoose = require('mongoose');
+var Company = require('./../models/Company.js');
+mongoose.connect('mongodb://localhost/dealbook-node-api');
+
+describe('Company', function() {
+  it('must have a name', function(done) {
+    Company.create({ name: 'Magnetis' }, function(err, company) {
+      expect(err).to.be.null;
+      expect(company.name).to.eql('Magnetis');
+      done();
+    });
+  });
+
+  it('throws error if has no name', function(done) {
+    Company.create({}, function(err, company) {
+      expect(err).not.to.be.null;
+      done();
+    });
+  });
+});
